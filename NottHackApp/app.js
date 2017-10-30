42522f40eca89673075a02d5189503b9efea4187
Jonathan Marsh <j.r.marsh@ntlworld.com>
2017-10-30 00:47:37 +0000

    Added error404 page Added favicon

--- NottHackApp/app.js
+++ NottHackApp/app.js
@@ -15,13 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+app.use(favicon(__dirname + '/public/favicon.ico'));
 
 if (app.get('env') === 'development') {
     app.use(function (err, req, res, next) {
@@ -32,17 +26,11 @@ if (app.get('env') === 'development') {
         });
     });
 }
-
-app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
+});
+app.get('*', function (req, res) {
+    res.sendFile(__dirname + '/public/error.html');
 });
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), function () {
