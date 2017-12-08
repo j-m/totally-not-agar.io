baee6dea0d8c263f4602e7a4e541f4d713d1d86c
Jonathan Marsh <j.r.marsh@ntlworld.com>
2017-12-08 19:42:07 +0000

    Working server

---
+++ NottHackApp/!Server Setup/app.js
@@ -0,0 +1,163 @@
+﻿'use strict';
+var debug = require('debug'),
+    logger = require('morgan'),
+    uuid = require('uuid4'),
+    WebSocketServer = require('ws').Server,
+    socket = new WebSocketServer({ port: 8080 });
+  
+function send(client, data) {
+    try { client.send(data); } catch (e) { console.log("Failed to send data to a client"); }
+}
+socket.on('connection', function (client) {
+    client.id = uuid();
+    console.log('Client '+client.id+' joined');
+    client.on('message', function (message){
+        var object;
+        try {
+            object = JSON.parse(message);
+        } catch (e) {
+            send(client, JSON.stringify({ "function": "error", "error": "Invalid JSON recieved from client. Raw data: " + message }));
+            return;
+        }
+        switch (object["function"]) {
+            case "play":
+                if (addPlayer(client.id, object["name"], object["fill"]) === true) send(client, JSON.stringify({ "function": "error", "error": "You seem to be already playing" }));
+                break;
+            case "move":
+                if (movePlayer(client.id, object["keys"]) === true) send(client, JSON.stringify({ "function": "error", "error": "You don't appear to be playing" }));
+                break;
+            default: send(client, JSON.stringify({ "function": "error", "error": "Unknown function recieved from client" })); break;
+        }
+    });
+});
+var world = {
+    minx: 0,
+    miny: 0,
+    maxx: 5000,
+    maxy: 2000,
+    foods: 150,
+    spikes: 20
+};
+var players = [], foods = [], spikes = [];
+function checkAlive() {
+    var newplayers = [];
+    socket.clients.forEach(function (client) { newplayers[client.id] = players[client.id]; });
+    for (var id in players) {
+        if (!newplayers[id])
+            console.log('Client ' + id + ' disconnected');
+    }
+    players = newplayers;
+}
+setInterval(checkAlive, 10000);
+function update() {
+    var newplayers = [];
+    for (var id in players) {
+        if (players[id]) {
+            var speed = 10 - 1 / players[id].mass;
+            if (players[id].y - 4 > world.miny) if (players[id].w === true) players[id].y -= speed;
+            if (players[id].x - 4 > world.minx) if (players[id].a === true) players[id].x -= speed;
+            if (players[id].y + 4 < world.maxy) if (players[id].s === true) players[id].y += speed;
+            if (players[id].x + 4 < world.maxx) if (players[id].d === true) players[id].x += speed;
+            newplayers.push(players[id]);
+        }
+    }
+    foods.forEach(function (food) {
+        food.offset++;
+        if (food.offset > 50) food.offset = -50;
+        for (var id in players) if (players[id])
+            if (Math.hypot(players[id].x - food.x, players[id].y - food.y) <= players[id].mass + food.mass) {
+                players[id].mass += 1;
+                food.spawn();
+            }
+    });
+    spikes.forEach(function (spike) {
+        spike.offset++;
+        if (spike.offset > 50) spike.offset = -50;
+    });
+    socket.clients.forEach(function each(client) {
+        send(client, JSON.stringify({ "function": "update", "players": newplayers, "foods": foods, "spikes": spikes, "playerx": players[client.id] ? players[client.id].x : 0, "playery": players[client.id] ? players[client.id].y : 0, "playermass": players[client.id] ? players[client.id].mass : 0}));
+    });
+} update();
+setInterval(update, 30);
+function calculateBorder(colour) {
+    var c = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(colour);
+    var r = parseInt(c[1], 16) - 32 > 0 ? parseInt(c[1], 16) - 32 : 0;
+    var g = parseInt(c[2], 16) - 32 > 0 ? parseInt(c[2], 16) - 32 : 0;
+    var b = parseInt(c[3], 16) - 32 > 0 ? parseInt(c[3], 16) - 32 : 0;
+    return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+function addPlayer(id, name, fill) {
+    var found = false;
+    players.forEach(function (player) {
+        if (player.id === id)
+            found = true;
+    });
+    if (found === false)
+        players[id] = { name: name, fill: fill, border: calculateBorder(fill), mass: 100, x: 150, y: 150, w: false, a: false, s: false, d: false };
+    else return true;
+}
+function movePlayer(id, keys) {
+    if (players[id]) {
+        players[id].w = keys.w;
+        players[id].a = keys.a;
+        players[id].s = keys.s;
+        players[id].d = keys.d;
+    }
+    else return true;
+}
+var colours = ["#00bfff", "#00ff00", "#ffff66", "#ff9900", "#9900cc", "#ff0000"];
+function spikeCreate() {
+    return {
+        x: Math.random() * (world.maxx - world.minx) + world.minx,
+        y: Math.random() * (world.maxy - world.miny) + world.miny,
+        offset: Math.random() * 100 - 50,
+        mass: 150
+    };
+}
+function spikesAdd() {
+    var newspike = spikeCreate(), attempts = 0, valid = false;
+    while (valid === false && attempts < 5) {
+        attempts++;
+        valid = true;
+        newspike.x = Math.random() * (world.maxx - world.minx) + world.minx;
+        newspike.y = Math.random() * (world.maxy - world.miny) + world.miny;
+        for (var i in players) if (players.hasOwnProperty(i)) {
+            if (Math.hypot(newspike.x - players[i].x, newspike.y - players[i].y) <= players[i].radius + 20) {
+                valid = false;
+            }
+        }
+    }
+    spikes.push(newspike);
+    if (spikes.length < world.spikes)
+        spikesAdd();
+} spikesAdd();
+
+function foodAdd() {
+    if (foods.length < world.foods) {
+        var colour = colours[Math.floor(Math.random() * (colours.length - 1))];
+        foods.push({
+            x: Math.random() * (world.maxx - world.minx) + world.minx,
+            y: Math.random() * (world.maxy - world.miny) + world.miny,
+            mass: 10,
+            fill: colour,
+            border: calculateBorder(colour),
+            offset: Math.random() * 100 - 50,
+            spawn: function () {
+                var attempts = 0, valid = false;
+                while (valid === false && attempts < 5) {
+                    attempts++;
+                    valid = true;
+                    this.x = Math.random() * (world.maxx - world.minx) + world.minx;
+                    this.y = Math.random() * (world.maxy - world.miny) + world.miny;
+                    for (var i in players)
+                        if (players.hasOwnProperty(i))
+                            if (Math.hypot(this.x - players[i].x, this.y - players[i].y) <= players[i].radius + 20)
+                                valid = false;
+                }
+            }
+        });
+        foodAdd();
+    }        
+}
+foodAdd();
+foods.forEach(function (food) { food.spawn(); });
---
+++ NottHackApp/!Server Setup/html/javascript/client.js
@@ -0,0 +1,163 @@
+﻿window.requestAnimFrame = function () {
+    return (
+        window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        function (callback ) {
+            window.setTimeout(callback, 1000 / 60);
+        }
+    );
+}();
+$(document).ready(function () {
+    var canvas = $("canvas")[0], context = canvas.getContext('2d'),
+        camera = { x: 0, y: 0 },
+        players = [], spikes = [], foods = [],
+        me = { mass: 100 };
+    function drawFood(food) {
+        context.beginPath();
+        context.arc(food.x - camera.x, food.y - camera.y, food.mass + (1 - Math.abs(food.offset) / 50)*2, 0, 2 * Math.PI, false);
+        context.closePath();
+        context.fillStyle = food.fill;
+        context.fill();
+        context.lineWidth = food.mass/2;
+        context.strokeStyle = food.border;
+        context.stroke();
+    }
+    function sineCircleXYatAngle(cx, cy, radius, amplitude, angle, sineCount) {
+        var x = cx + (radius + amplitude * Math.sin(sineCount * angle)) * Math.cos(angle);
+        var y = cy + (radius + amplitude * Math.sin(sineCount * angle)) * Math.sin(angle);
+        return { x: x, y: y };
+    }
+    function drawSpike(spike) {
+        context.fillStyle = '#00ff00';
+        context.lineWidth = spike.mass / 15;
+        context.strokeStyle = '#00df00';
+        context.beginPath();
+        for (var i = 0; i < 360; i++) {
+            var angle = i * Math.PI / 180;
+            var pt = sineCircleXYatAngle(spike.x - camera.x, spike.y - camera.y, spike.mass,  spike.mass/15 + (1 - Math.abs(spike.offset) / 10), angle + (1 - Math.abs(spike.offset) / 1000), 50);
+            context.lineTo(pt.x, pt.y);
+        }
+        context.closePath();
+        context.fill();
+        context.stroke();
+    }
+    function drawPlayer(player) {
+        context.beginPath();
+        context.arc(player.x - camera.x, player.y - camera.y, player.mass, 0, 2 * Math.PI, false);
+        context.fillStyle = player.fill;
+        context.fill();
+        context.lineWidth = 10;
+        context.strokeStyle = player.border;
+        context.stroke();
+        
+        context.textAlign = "center";
+        context.textBaseline = "middle";
+        context.font = player.mass/5 + "px Sans-serif";
+
+        context.lineWidth = 5;
+        context.strokeStyle = '#111';
+        context.strokeText(player.name, player.x - camera.x, player.y - camera.y);
+
+        context.fillStyle = 'white';
+        context.fillText(player.name, player.x - camera.x, player.y - camera.y);
+
+        context.font = player.mass/10 + "px Sans-serif";
+        context.strokeText(player.mass, player.x - camera.x, player.y - camera.y - player.mass / 5);
+        context.fillText(player.mass, player.x - camera.x, player.y - camera.y - player.mass / 5);
+    }
+    var zoom = 1;
+    function grid() {
+        context.beginPath();
+        context.lineWidth = 1;
+        context.strokeStyle = '#EEE';
+        for (x = 0; x <= parseInt(canvas.style.width * zoom, 10) + 1; x += parseInt(canvas.style.width * zoom, 10) / 20) {
+            context.moveTo(Math.floor(x - camera.x % 20), 0);
+            context.lineTo(Math.floor(x - camera.x % 20), parseInt(canvas.style.height, 10) );
+            for (y = 0; y <= parseInt(canvas.style.height * zoom, 10) + 1; y += parseInt(canvas.style.width * zoom, 10) / 20) {
+                context.moveTo(0, Math.floor(y - camera.y % 20));
+                context.lineTo(parseInt(canvas.style.width, 10) , Math.floor(y - camera.y % 20));
+            }
+        }
+        context.stroke();
+    }
+    function update(data) {
+        players = data["players"];
+        spikes = data["spikes"];
+        foods = data["foods"];
+        me.mass = data["playermass"]; 
+
+        $('#players').text(players.length + (players.length !== 1 ? ' Players ' : ' Player ') + 'In-game');
+
+        zoom = me.mass * 2 / (parseInt(canvas.style.width, 10) / 5);
+        canvas.width = parseInt(canvas.style.width, 10) * zoom;
+        canvas.height = parseInt(canvas.style.height, 10) * zoom;
+        camera.x = data["playerx"] - canvas.width / 2;
+        camera.y = data["playery"] - canvas.height / 2;
+
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        grid();
+        foods.forEach(function (food) { drawFood(food); });
+        players.forEach(function (player) { drawPlayer(player); });
+        spikes.forEach(function (spike) { drawSpike(spike); });
+    }
+    function error(description) {
+        console.log("ERROR: " + description);
+        $('#error').text("ERROR: " + description);
+        $('#error').css('visibility', 'visible');
+    }
+    ws = new WebSocket("ws://139.59.182.80:8080/");
+    ws.onerror = function (evt) { error(evt.data); };
+    ws.onmessage = function (message) {
+        var object;
+        try {
+            object = JSON.parse(message.data);
+        } catch (e) {
+            error("Invalid JSON recieved from server. Raw data: " + message.data);
+            return;
+        }
+        switch (object["function"]) {
+            case "update": update(object); break;
+            case "error": error(object["error"]); break;
+            default: error("Unknown function recieved from server: " + object["function"]); break;
+        }
+    };
+    function draw() {
+        if (ws.readyState !== ws.CLOSED) {
+            context.drawImage(canvas, 0, 0);
+            requestAnimFrame(draw);
+        } else { error(" Connection closed"); }
+    } draw();
+    $('form').submit(function (e) {
+        e.preventDefault();
+        ws.send(JSON.stringify({
+            "function": "play",
+            "name": $('#name').val(),
+            "fill": $('#colour').val()
+        }));
+        $('form').hide();
+        $("canvas").attr("tabindex", "0");
+        $("canvas").focus();
+        return false;
+    });
+    var keys = { w: false, a: false, s: false, d: false };
+    $("canvas").keydown(function (e) {
+        var key = e.which;
+        if (key === 65) keys.a = true;
+        else if (key === 87) keys.w = true;
+        else if (key === 68) keys.d = true;
+        else if (key === 83) keys.s = true;
+        ws.send(JSON.stringify({ "function": "move", "keys":keys }));
+    });
+
+    $("canvas").keyup(function (e) {
+        var key = e.which;
+        if (key === 65) keys.a = false;
+        else if (key === 87) keys.w = false;
+        else if (key === 68) keys.d = false;
+        else if (key === 83) keys.s = false;
+        ws.send(JSON.stringify({ "function": "move", "keys": keys }));
+    });
+});
